0a068948ea777e8ab43602aedcca6849f6903383
mat <54773138+mateis-rgb@users.noreply.github.com>
2024-12-30 16:33:38 +0100

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Mateis Ragon on 30/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
